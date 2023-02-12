b00d73dda0bdd7478bc0e49b6a73f710545812b4
Laxmikant <dwivedilakshya7509@gmail.com>
2023-02-12 23:36:24 +0530

    third sort012.cpp file

---
+++ sort012.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+       int i=0;
+       int j=n-1;
+       int mid = 0;
+       while(mid<=j)
+       {
+           if(a[mid] == 0)            // for zero
+           {
+               swap(a[mid],a[i]);
+               mid++;
+               i++;
+           }
+           else if(a[mid] == 1){     // for one
+               mid++;
+           }
+           else
+           {
+               swap(a[mid],a[j]);    // for 2
+               j--;
+           }
+       }
+    }
+}
