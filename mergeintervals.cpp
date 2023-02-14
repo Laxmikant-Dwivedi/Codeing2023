70cbc18aa19024f8e334aadb7ebdfb0dacb77a87
Laxmikant <dwivedilakshya7509@gmail.com>
2023-02-15 03:32:55 +0530

    Added merge.cpp

---
+++ mergeintervals.cpp
@@ -0,0 +1,19 @@
+vector<vector<int>> mergeintervals(vector<int> intervals)
+{
+    vector<vector<int>> mergeintervals;
+    if(intervals.size() == 0 ) return mergeintervals;
+    vector<int> tempintervals = interval[0];
+    for(auto it: intervals)
+    {
+        if(it[0] <= intervals[1])
+        {
+            intervals[1] = max(it[1],intervals[1]);
+        }
+        else{
+            mergeintervals.push_back(tempintervals);
+            tempintervals = it;
+        }
+    } 
+    mergeintervals.push_back(tempintervals);
+    return mergeintervals;
+}
