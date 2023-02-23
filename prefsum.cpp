f2335c2afad29fa9fbfa10577c1f2adefac6b142
Laxmikant <dwivedilakshya7509@gmail.com>
2023-02-23 23:48:41 +0530

    Master Updated

--- prefsum.cpp
+++ prefsum.cpp
@@ -2,6 +2,7 @@ bool subArrayExists(int arr[], int n){
     unordered_set<int> s;
     for(int i=0; i<n; i++){
         int prefixsum = 0;
+        arr[i] += prefixsum;
         if(arr[i] == 0)
             return 1;
         if(s.find(prefixsum) != s.end())
